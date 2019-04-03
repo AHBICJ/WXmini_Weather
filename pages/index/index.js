79a7b3d16b0be3eb2ab5713d5aaaecfe1e8a1a81
AHBICJ <810988732@qq.com>
2019-04-04 01:25:57 +0800

    add weather api request

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,23 @@
-Page({})
+Page({
+  onLoad(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '绍兴市'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: res => console.log(res),
+    })
+  }
+
+});
+wx.setNavigationBarColor({
+  frontColor: '#ffffff',
+  backgroundColor: '#C4EFFF',
+  animation: {
+    duration: 400,
+    timingFunc: 'easeIn'
+  }
+})

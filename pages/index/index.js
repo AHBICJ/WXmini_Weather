67cc284ebff4f76f260870d79fbfbe2c5acfc8b2
AHBICJ <810988732@qq.com>
2019-04-04 18:25:23 +0800

    Dynamic update weather
    Dynamic update of weather include background image and navigation bar color

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,27 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+  'sunny': '#C4EFFF',
+  'cloudy': '#DAEFF7',
+  'overcast': '#C4CED2',
+  'lightrain': '#B6D6E2',
+  'heavyrain': '#C3CCD0',
+  'snow': '#99E3FF'
+}
+
 Page({
+  data:{
+    nowTemp:'',
+    nowWeather:'',
+    nowWeatherBackground:'',
+  },
   onLoad(){
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
@@ -8,16 +31,25 @@ Page({
       header: {
         'content-type': 'application/json'
       },
-      success: res => console.log(res),
+      success: res => {
+        const result = res.data.result;
+        let temp = result.now.temp;
+        let weather = result.now.weather;
+        this.setData({
+          nowTemp: temp + "°",
+          nowWeather:weatherMap[weather],
+          nowWeatherBackground: `/images/${weather}-bg.png`,
+        });
+        wx.setNavigationBarColor({
+          frontColor: '#ffffff',
+          backgroundColor: weatherColorMap[weather],
+          animation: {
+            duration: 400,
+            timingFunc: 'easeIn'
+          }
+        })
+      }
     })
   }
 
 });
-wx.setNavigationBarColor({
-  frontColor: '#ffffff',
-  backgroundColor: '#C4EFFF',
-  animation: {
-    duration: 400,
-    timingFunc: 'easeIn'
-  }
-})
